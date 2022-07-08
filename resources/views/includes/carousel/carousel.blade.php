ae9b33cf0c25df4bd78ea6c96618817a3317f23e
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-07 20:18:26 -0500

    fixed errors in css animation

--- resources/views/includes/carousel/carousel.blade.php
+++ resources/views/includes/carousel/carousel.blade.php
@@ -1,19 +1,19 @@
 <div class="container mx-auto flex items-center justify-center overflow-hidden">
     <div id="slider" class="relative h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400">
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/BlackwellSwamp.jpg') }}" alt="" />
+            <img class="w-full object-cover h-[350px]" src="{{ url('storage/images/BlackwellSwamp.jpg') }}" alt="" />
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/BurgreenFarmPurpleClover.jpg')}}" alt="" />
+            <img class="w-full object-cover h-[350px]" src="{{ url('storage/images/BurgreenFarmPurpleClover.jpg') }}" alt="" />
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/CottonField.jpg')}}" alt="">
+            <img class="w-full object-cover h-[350px]" src="{{ url('storage/images/CottonField.jpg') }}" alt="">
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/CottonOnCountyLineRoad.jgp')}}" alt="">
+            <img class="w-full object-cover h-[350px]" src="{{ url('storage/images/CottonOnCountyLineRoad.jpg') }}" alt="">
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/FamilyGraveyard.jpg')}}" alt="">
+            <img class="w-full object-cover h-[350px]" src="{{ url('storage/images/FamilyGraveyard.jpg') }}" alt="">
         </div>
     </div>
 </div>
