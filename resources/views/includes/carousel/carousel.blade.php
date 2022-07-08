b2600493d94923444e885bf85cf530c7919b5815
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-07 21:45:02 -0500

    added some height to Carousel div

--- resources/views/includes/carousel/carousel.blade.php
+++ resources/views/includes/carousel/carousel.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid mx-auto flex items-center justify-center overflow-hidden">
+<div class="container-fluid mx-auto flex items-center justify-center overflow-hidden h-[600px]">
     <div id="slider" class="relative h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400">
         <div class="slide absolute">
             <img class="w-full object-cover h-[316px]" src="{{ url('storage/images/BlackwellSwamp.jpg') }}" alt="" />
