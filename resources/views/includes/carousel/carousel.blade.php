c673512edec2335e209ffc179180354861f4b59b
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-07 20:06:47 -0500

    Added sample images

--- resources/views/includes/carousel/carousel.blade.php
+++ resources/views/includes/carousel/carousel.blade.php
@@ -1,19 +1,19 @@
 <div class="container mx-auto flex items-center justify-center overflow-hidden">
     <div id="slider" class="relative h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400">
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg" alt="" />
+            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/BlackwellSwamp.jpg') }}" alt="" />
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="https://cdn.pixabay.com/photo/2017/10/20/10/58/elephant-2870777__340.jpg" alt="" />
+            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/BurgreenFarmPurpleClover.jpg')}}" alt="" />
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="https://cdn.pixabay.com/photo/2015/02/20/16/59/deer-643340__340.jpg" alt="">
+            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/CottonField.jpg')}}" alt="">
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="https://cdn.pixabay.com/photo/2018/05/17/09/18/away-3408119__340.jpg" alt="">
+            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/CottonOnCountyLineRoad.jgp')}}" alt="">
         </div>
         <div class="slide absolute">
-            <img class="w-full object-cover h-[400px]" src="https://cdn.pixabay.com/photo/2018/03/16/20/13/seagull-3232350__340.jpg" alt="">
+            <img class="w-full object-cover h-[400px]" src="{{url('storage/images/FamilyGraveyard.jpg')}}" alt="">
         </div>
     </div>
 </div>
