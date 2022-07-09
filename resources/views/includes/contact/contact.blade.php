037dbccee207558a18fc36cb371987fd9437c126
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-09 14:06:13 -0500

    changed text styling for p tags

--- resources/views/includes/contact/contact.blade.php
+++ resources/views/includes/contact/contact.blade.php
@@ -5,16 +5,16 @@
             <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
         </div>
         <div>
-            <h4 class="mb-5 text-xl font-bold">Purchasing work from Jim Felder:</h4>
-            <p>The paintings on this site are classified as Available Work (work that is for sale) and Portfolio
+            <h4 class="mb-5 text-2xl font-bold">Purchasing work from Jim Felder:</h4>
+            <p class="text-xl">The paintings on this site are classified as Available Work (work that is for sale) and Portfolio
                 (work that is already owned). If you are interested in a painting in the Portfolio section, please
                 let me know of your interest in case I will be revisiting the subject at some time in the future or
                 I have plans to produce a similar work. </p>
-            <p>If you are interested in purchasing Available Work, contact me for price and details. I will be happy
+            <p class="text-xl">If you are interested in purchasing Available Work, contact me for price and details. I will be happy
                 to discuss any aspect of my work with you.</p>
             <address class="mt-5">
-                <p>Phone/text: 256.426.0558</p>
-                <p>Email: jim.felder@gmail.com</p>
+                <p class="text-xl">Phone/text: 256.426.0558</p>
+                <p class="text-xl">Email: jim.felder@gmail.com</p>
             </address>
         </div>
     </div>
