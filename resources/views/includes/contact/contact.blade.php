33de79edd3c819f1f81d88d889de1d65c016ce1b
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-09 21:02:58 -0500

    Added some padding to the right on some of the text in biography and contact

--- resources/views/includes/contact/contact.blade.php
+++ resources/views/includes/contact/contact.blade.php
@@ -1,22 +1,30 @@
-<section id="contact" class="container-fluid m-5 p-5 snap-always snap-start pt-52 pb-56">
-    <h2 class="text-center text-6xl m-5 p-5">Contact Info</h2>
-    <div class="grid grid-cols-2 justify-center mx-auto">
-        <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-            <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
+<section id = "contact" class = " m-5 p-5 snap-always snap-start pt-52 pb-56">
+    <h2 class = "text-center text-6xl m-5 p-5">Contact Info</h2>
+    <div class = "grid grid-cols-2 justify-center mx-auto pt-10">
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
         </div>
         <div>
-            <h4 class="mb-5 text-2xl font-bold">Purchasing work from Jim Felder:</h4>
-            <p class="text-xl">The paintings on this site are classified as Available (work that is for sale) in the
-                notes that accompany each image. If you are interested an available work, please let me know of your
-                interest in case I will be revisiting the subject at some time in the future or I have plans to produce
+            <h4 class = "mb-5 text-2xl font-bold">Purchasing work from Jim Felder:</h4>
+            <p class = "text-xl pr-20">
+                The paintings on this site are classified as Available (work that is for sale) in the
+                notes that accompany each image. If you are interested an available work, please let me
+                know of your
+                interest in case I will be revisiting the subject at some time in the future or I have
+                plans to produce
                 a similar work.</p>
-            <p class="text-xl">If you are interested in purchasing Available work, contact me for price and details.
-                I will be happy to discuss any aspect of my work with you at any time regardless of your intent
+            <p class = "text-xl pr-20">
+                If you are interested in purchasing Available work, contact me for price and details.
+                I will be happy to discuss any aspect of my work with you at any time regardless of
+                your intent
                 to purchase.
             </p>
-            <address class="mt-5">
-                <p class="text-xl">Phone/text: 256.426.0558</p>
-                <p class="text-xl">Email: jim.felder@gmail.com</p>
+            <address class = "mt-5">
+                <p class = "text-xl">Phone/text: 256.426.0558</p>
+                <p class = "text-xl">Email: jim.felder@gmail.com</p>
             </address>
         </div>
     </div>
