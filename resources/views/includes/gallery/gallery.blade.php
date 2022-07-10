a921be938cf2a864eaafdbc2721dacf77f445d71
Thomas Bockhorn <historian.thomas@gmail.com>
2022-07-09 22:53:02 -0500

    Changed the format of the gallery to two columns

--- resources/views/includes/gallery/gallery.blade.php
+++ resources/views/includes/gallery/gallery.blade.php
@@ -1,91 +1,217 @@
-<section class="grid grid-cols-4 gap-16">
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
+<section class = "flex justify-center">
+    <div class = "grid grid-cols-2 gap-16">
+        <!-- Picture 1 -->
+        <figure
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/BlackwellSwampBlueHole.jpg') }}"
+                 alt = "Blackwell Swamp Blue Hole">
+        </figure>
+        <div>
+            <p>Title: <em>Blackwell Swamp Blue Hole</em></p>
+            <p>Size: 10 x 14</p>
+            <p>Medium: Oil on panel</p>
+            <p>Location: Madison County, Alabama</p>
+            <p>Status: Unframed. Available.</p>
+            <p>Notes: Thanksgiving Day 2020</p>
+        </div>
+
+
+        <!-- Picture 2 -->
+        <figure
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/BlackwellSwamp.jpg') }}"
+                 alt = "Blackwell Swamp">
+        </figure>
+        <div>
+            <p>Title: <em>Blackwell Swamp</em></p>
+            <p>Size: 32 x 74</p>
+            <p>Medium: Oil on panel</p>
+            <p>Location: Madison County, Alabama</p>
+            <p>Status: Unframed. Not available.</p>
+            <p>Notes:</p>
+        </div>
+
+        <!-- Picture 3 -->
+        <figure
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/BrownHeadedNutHatch.jpg') }}"
+                 alt = "Brown Headed Nut Hatch">
+        </figure>
+        <div>
+            <p>Title: <em>Brown Headed Nuthatch</em></p>
+            <p>Size: 24 x 24</p>
+            <p>Medium: Oil on panel</p>
+            <p>Location: Madison County, Alabama</p>
+            <p>Status: Framed. Not available.</p>
+            <p>Notes:</p>
+        </div>
+
+
+        <!-- Picture 4 -->
+        <figure
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/BurgreenFarmRedClover.jpg') }}"
+                 alt = "Burgreen Farm Red Clover">
+        </figure>
+        <div>
+            <p>Title: <em>Burgreen Farm Red Clover</em></p>
+            <p>Size: 10 x 14</p>
+            <p>Medium: Oil on panel</p>
+            <p>Location: Browns Ferry Road, Limestone County Alabama</p>
+            <p>Status: Unframed. Available.</p>
+            <p>Notes:</p>
+        </div>
+
+        <!-- Picture 5 -->
+        <figure
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/CottonFieldneartheTennesseeRiverMadisonCounty.jpg') }}"
+                 alt = "Cotton Field near the Tennessee River, Madison County">
+        </figure>
+        <div>
+            <p>Title: <em>Tennessee River Cottonfield</em></p>
+            <p>Size: 11 x 14</p>
+            <p>Medium: Watercolor</p>
+            <p>Location: Madison County Alabama off County Line Road</p>
+            <p>Status: Unframed. Available.</p>
+            <p>Notes:</p>
+        </div>
+
+
+        <!-- Picture 6 -->
+        <figure
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/CottonOperationonCountyLineRoad.jpg') }}"
+                 alt = "Cotton Operation on County Line Road">
+        </figure>
+        <div>
+            <p>Title: <em>Cotton Operation on County Line Road</em></p>
+            <p>Size: 16 x 22</p>
+            <p>Medium: Watercolor</p>
+            <p>Location: Madison County Alabama</p>
+            <p>Status: Framed. Not available.</p>
+            <p>Notes:</p>
+        </div>
+
+        <!-- Picture 7 -->
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div
+            class = "h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
+            <img class = "object-fill w-full h-full absolute"
+                 src = "{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}"
+                 alt = "Over grown house on Browns Ferry Road">
+        </div>
+        <div></div>
+        <div></div>
     </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div class="h-80 w-96 border-black border-2 shadow-xl rounded shadow-gray-400 relative overflow-hidden mx-auto ">
-        <img class="object-fill w-full h-full absolute" src="{{ url('storage/images/OvergrownBrownsFerryRoad.jpg') }}" alt="Over grown house on Browns Ferry Road">
-    </div>
-    <div></div>
-    <div></div>
 </section>
